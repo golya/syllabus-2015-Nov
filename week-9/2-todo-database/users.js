2fb195f626fcf7e3cac65244f4318fddf40267d0
Ádám Gólya <adam.stork@gmail.com>
2016-01-20 11:24:42 +0100

    feat(users.js): add few methods for being an example for todo app
    Signed-off-by: Ádám Gólya <adam.stork@gmail.com>

--- week-9/2-todo-database/users.js
+++ week-9/2-todo-database/users.js
@@ -15,14 +15,37 @@ function addUser(attributes) {
   });
 }
 
-function getUser(attributes) {
+function updateUser(attributes) {
+  connection.query('UPDATE user SET ?', attributes, function(err, result) {
+    if (err) throw err;
+    console.log(result.changedRows);
+  });
+}
+
+function delUser(attributes) {
+  connection.query('DELETE FROM user WHERE id=1', function(err, result) {
+    if (err) throw err;
+    console.log(result);
+  });
+}
+
+function getUser() {
   connection.query('SELECT * FROM `user`', function(err, results) {
     if (err) throw err;
     console.log(results);
   });
 }
 
+function getUserById(attributes, cb) {
+  connection.query('SELECT * FROM `user` WHERE user_id=?', [attributes.user_id], function(err, results) {
+    if (err) throw err;
+    console.log(results);
+    cb(results);
+  });
+}
+
 module.exports = {
   add: addUser,
-  get: getUser
+  get: getUser,
+  getUserById: getUserById
 };
